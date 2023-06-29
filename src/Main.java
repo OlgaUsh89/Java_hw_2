ea5e7898e5d1787e634b5401dc4c7d34cb69200c
Olya test <ushakovaolga1989@gmail.com>
2023-06-29 22:24:34 +0300

    Calculation of bonus miles added

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,8 @@
 public class Main {
+    public static void main(String[] args) {
+        int price = 5000;
+        int amount = 20;
+
+        System.out.println((price / amount) + " бонусных миль");
+    }
 }
